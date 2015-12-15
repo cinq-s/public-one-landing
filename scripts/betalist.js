6fa50a44b7b9e8b4743197f657d4b522bc0b7926
Ethan Sebban <ethan.sebban@gmail.com>
2015-12-15 18:28:37 +0100

    Hack mime type

--- scripts/betalist.js
+++ scripts/betalist.js
@@ -5,7 +5,7 @@ var MyBetaList =
 {
   // Properties
   isSubmitted: false,
-  defaultEmailAddress: 'email@example.com',
+  defaultEmailAddress: 'email@exemple.com',
   referral: null,
   emailRegex: /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,
   url: null,
@@ -54,12 +54,8 @@ var MyBetaList =
     }
   },
 
-  showStep2: function(referral_link)
+  showStep2: function()
   {
-    // Set referral link
-    jQuery('#MyBetaList-referrer-link').attr('href', referral_link);
-    jQuery('#MyBetaList-referrer-link').html(referral_link);
-
     // Hide step 1
     jQuery('#MyBetaList-step1').hide();
 
@@ -120,19 +116,20 @@ var MyBetaList =
 
         // Send email address
         // @TODO: Use callback? (XSS)
-        jQuery.getJSON('https://my.betali.st/subscribe?callback=?', params, function(data) {
-          if(data.result)
-          {
-            // Show step 2
-            MyBetaList.showStep2(data.result);
-          }
-          else
-          {
-            // Something went wrong (e.g. non-existing token)
-            alert('Whoops. Couldn\'t add new subscriber. Please contact site owner.');
-          }
-        });
-
+        // jQuery.getJSON('https://my.betali.st/subscribe?callback=?', params, function(data) {
+        //   if(data.result)
+        //   {
+        //     // Show step 2
+        //     MyBetaList.showStep2(data.result);
+        //   }
+        //   else
+        //   {
+        //     // Something went wrong (e.g. non-existing token)
+        //     alert('Whoops. Couldn\'t add new subscriber. Please contact site owner.');
+        //   }
+        // });
+        jQuery.getJSON('https://my.betali.st/subscribe?callback=?', params, function() {});
+        MyBetaList.showStep2();
       }
 
     });
